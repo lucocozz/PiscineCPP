79c04acdc358af3a746faa41fe61030cf5908bac
lucocozz <lucocozz@student.42.fr>
2021-10-28 14:00:48 +0200

    wip

---
+++ module01/ex05/includes/Karen.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Karen.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 01:58:50 by lucocozz          #+#    #+#             */
+/*   Updated: 2021/10/28 02:25:17 by lucocozz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <iostream>
+
+class Karen
+{
+private:
+	void	debug(void);
+	void	info(void);
+	void	warning(void);
+	void	error(void);
+public:
+	Karen();
+	~Karen();
+	void complain(std::string level);
+};
