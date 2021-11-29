660f52f9c1b678a8f09d5ad0753b74e2394ca1b4
lucocozz <lcocozza.42@gmail.com>
2021-11-29 16:09:34 +0100

    minor change

--- module03/ex03/sources/FragTrap.cpp
+++ module03/ex03/sources/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 20:23:35 by lucocozz          #+#    #+#             */
-/*   Updated: 2021/11/20 16:40:23 by lucocozz         ###   ########.fr       */
+/*   Updated: 2021/11/29 16:01:56 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ FragTrap::FragTrap(std::string name): ClapTrap(name)
 {
 	std::cout << "Constructor FragTrap " << name << std::endl;
 	this->_hitPoints = 100;
-	this->_energyPoints = 50;
-	this->_attackDamage = 20;
+	this->_energyPoints = 100;
+	this->_attackDamage = 30;
 }
 
 FragTrap::FragTrap(FragTrap &copy)
